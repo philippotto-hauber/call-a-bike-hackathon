49cf2e74b1d75e4c0e39457d973d11a56c32cee6
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-04-01 22:51:39 +0200

    comment out bookings data

--- download_data.py
+++ download_data.py
@@ -1,10 +1,12 @@
 import requests, zipfile, io
 
 url_base = 'https://download-data.deutschebahn.com/static/datasets/callabike/20170516/'
-file_names = ['OPENDATA_CATEGORY_CALL_A_BIKE',
-              'OPENDATA_RENTAL_ZONE_CALL_A_BIKE',
-              'OPENDATA_VEHICLE_CALL_A_BIKE',
-              'OPENDATA_BOOKING_CALL_A_BIKE']
+file_names = [
+                'OPENDATA_CATEGORY_CALL_A_BIKE',
+                'OPENDATA_RENTAL_ZONE_CALL_A_BIKE',
+                'OPENDATA_VEHICLE_CALL_A_BIKE',
+                #'OPENDATA_BOOKING_CALL_A_BIKE' #commented out because data not available under the url
+            ]
 file_extension = '.zip'
 
 for f in file_names:
