6cbc114b2f534613be2820ffa5b1c07eb5ed2b05
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-04-01 21:54:11 +0200

    add script to download and unzip data

---
+++ download_data.py
@@ -0,0 +1,13 @@
+import requests, zipfile, io
+
+url_base = 'https://download-data.deutschebahn.com/static/datasets/callabike/20170516/'
+file_names = ['OPENDATA_CATEGORY_CALL_A_BIKE',
+              'OPENDATA_RENTAL_ZONE_CALL_A_BIKE',
+              'OPENDATA_VEHICLE_CALL_A_BIKE',
+              'OPENDATA_BOOKING_CALL_A_BIKE']
+file_extension = '.zip'
+
+for f in file_names:
+    r = requests.get(url_base + f + file_extension)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(path = 'data')
