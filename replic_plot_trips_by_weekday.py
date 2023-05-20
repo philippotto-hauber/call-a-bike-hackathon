d3e3cab28070a4c42814a05f5c2a672087decbdb
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-05-20 18:26:43 +0200

    add script to replicate plot of trips by weekday

---
+++ replic_plot_trips_by_weekday.py
@@ -0,0 +1,28 @@
+#%% libs
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+#%% load plot data
+dat_plot = pd.read_csv("./plots/data/trips_weekday/dat_trips_by_weekday.csv",
+                       index_col='interval')
+
+#%% plot
+
+# plot and color setting
+plt.style.use('seaborn')
+my_cmap = sns.color_palette(palette="flare", n_colors = 7, as_cmap=True)
+
+# actual plot
+ax = dat_plot.plot(colormap=my_cmap,
+                   title = "Number of bike rides by time of day",
+                   figsize=(15,8))
+
+# set xticks, labels, legend
+ax.set_xticks(ticks = range(0,24, 1), 
+              labels = list(map(lambda x: str(x) + ':00', 
+                            range(0, 24, 1))),
+              rotation = 0)
+ax.set_xlabel("")
+ax.legend(title = None, loc = 'upper left')
+
