f1e989f26289bbabf685a1a686428558ef80eacb
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-05-28 08:03:13 +0200

    update heatmap
    - left merge instead of inner to document station ids without name
    - should have no impact on the exported plot and data

--- plot_heatmap_trips.py
+++ plot_heatmap_trips.py
@@ -1,32 +1,54 @@
 #%% libs
 import pandas as pd
 import numpy as np
-#import matplotlib
-#import matplotlib as mpl
 import matplotlib.pyplot as plt
 import plotly.express as px
 
 #%% load data
-
 dat_raw = pd.read_csv('./data/biketrip_data.csv')
 dat_stations = dat_raw[['start_station_id', 'end_station_id', 'datetime_from']]
+del dat_raw
+dat_stations.info(show_counts=True)
 
 # %% load rental zone data to get full name of stations
 dat_rental_zone = pd.read_csv('./data/OPENDATA_RENTAL_ZONE_CALL_A_BIKE.csv', sep = ";", decimal = ",")
 
 #%% merge with dat_stations to get start/end_station_name column
-dat_stations = (dat_stations
-                .merge(dat_rental_zone[['RENTAL_ZONE_HAL_ID', 'NAME']], 
-                              left_on='start_station_id', 
-                              right_on = 'RENTAL_ZONE_HAL_ID')
-                .rename(columns = {'NAME': 'start_station_name'})
-                .drop(columns = ["RENTAL_ZONE_HAL_ID", "start_station_id"])
-                .merge(dat_rental_zone[['RENTAL_ZONE_HAL_ID', 'NAME']], 
-                       left_on='end_station_id', 
-                       right_on = 'RENTAL_ZONE_HAL_ID')
-                .rename(columns = {'NAME': 'end_station_name'})
-                .drop(columns = ["RENTAL_ZONE_HAL_ID", "end_station_id"])
-                )
+dat_stations_merged = (dat_stations
+                            .merge(dat_rental_zone[['RENTAL_ZONE_HAL_ID', 'NAME']], 
+                                   how = "left",
+                                   left_on='start_station_id', 
+                                   right_on = 'RENTAL_ZONE_HAL_ID')
+                            .rename(columns = {'NAME': 'start_station_name'})
+                            .drop(columns = ["RENTAL_ZONE_HAL_ID"])
+                            .merge(dat_rental_zone[['RENTAL_ZONE_HAL_ID', 'NAME']], 
+                                   how = 'left',
+                                   left_on='end_station_id', 
+                                   right_on = 'RENTAL_ZONE_HAL_ID')
+                            .rename(columns = {'NAME': 'end_station_name'})
+                            .drop(columns = ["RENTAL_ZONE_HAL_ID"])
+                            )
+dat_stations_merged.info(show_counts=True)
+
+#%%  station_ids for which no name can be found
+start_id_with_missings = (dat_stations_merged['start_station_id']
+                                   [dat_stations_merged['start_station_name']
+                            .isna()].
+                            unique()
+                            )
+
+end_id_with_missings = (dat_stations_merged['end_station_id']
+                                   [dat_stations_merged['end_station_name']
+                            .isna()].
+                            unique()
+                            )
+
+id_missing = np.unique([start_id_with_missings, end_id_with_missings])
+print(id_missing)
+
+#%% drop stations with no name
+dat_stations = dat_stations_merged.dropna().drop(columns = ["start_station_id", "end_station_id"])
+dat_stations.info(show_counts = True)
 
 #%% calculate number of trips for each station pair
 dat_stations = (dat_stations.groupby(['start_station_name', 'end_station_name'])
@@ -58,9 +80,10 @@ fig = px.imshow(dat_plot,
 fig.update_xaxes(showticklabels=False)
 fig.update_yaxes(showticklabels=False)
 fig.show()
+
+#%% export figure to html
 fig.write_html("./plots/heatmap_trips.html")
 
 #%% save data
-
 dat_plot.to_csv("./plots/data/trips_heatmap/dat_trips_heatmap.csv",
                 index=True)
